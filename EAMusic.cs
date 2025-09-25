d3b2c7dde06d80a675c36b55b6df5dd6220e42c4
Sernk <135448749+Sernk@users.noreply.github.com>
2025-09-25 09:46:01 +0300

    Main 2

--- EAMusic.cs
+++ EAMusic.cs
@@ -1,14 +1,19 @@
+using System;
 using Terraria.ModLoader;
 
 namespace EAMusic
 {
 	public class EAMusic : Mod
 	{
+        public static bool EARetroLoaded;
+
         public static string SoundSlot(string name)
         {
             return $"EAMusic/Sounds/Music/{name}";
         }
-        public static bool EALoaded;
-        public static bool EARetroLoaded;
+        public override void Load()
+        {
+            EARetroLoaded = ModLoader.TryGetMod("EARetroMusic", out Mod EARetroMusic);
+        }
     }
 }
--- EAUpdateMusic.cs
+++ EAUpdateMusic.cs
@@ -7,28 +7,33 @@ namespace EAMusic
     public class EAUpdateMusic: ModSceneEffect
     {
         int music;
+
         public override int Music => music;
         public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
         public override bool IsSceneEffectActive(Player player)
         {
-            if (Main.myPlayer != -1 && !Main.gameMenu && Main.LocalPlayer.active && Main.invasionX == (double)Main.spawnTileX && MyWorld.voidInvasionUp)
+            if (EAMusic.EARetroLoaded == false)
             {
-                if (!Main.dayTime && Main.time > 16220.0)
+                if (Main.myPlayer != -1 && !Main.gameMenu && Main.LocalPlayer.active && Main.invasionX == (double)Main.spawnTileX && MyWorld.voidInvasionUp)
                 {
-                    music = MusicLoader.GetMusicSlot(EAMusic.SoundSlot("DawnOfTheVoidTheme"));
+                    if (!Main.dayTime && Main.time > 16220.0)
+                    {
+                        music = MusicLoader.GetMusicSlot(EAMusic.SoundSlot("DawnOfTheVoidTheme"));
+                        return true;
+                    }
+                    else
+                    {
+                        music = MusicLoader.GetMusicSlot(EAMusic.SoundSlot("DawnOfTheVoidTheme1"));
+                        return true;
+                    }
+                }
+                if (Main.LocalPlayer.active && Main.invasionX == (double)Main.spawnTileX && MyWorld.radiantRain)
+                {
+                    music = MusicLoader.GetMusicSlot(EAMusic.SoundSlot("RadiantRainTheme"));
                     return true;
                 }
-                else
-                {
-                    music = MusicLoader.GetMusicSlot(EAMusic.SoundSlot("DawnOfTheVoidTheme1"));
-                    return true;
-                }
-            }
-            if (Main.LocalPlayer.active && Main.invasionX == (double)Main.spawnTileX && MyWorld.radiantRain)
-            {
-                music = MusicLoader.GetMusicSlot(EAMusic.SoundSlot("RadiantRainTheme"));
-                return true;
-            }
+                return false;
+            }      
             else return false;
         }
     }
